ef971595f9a4ad7013a547a5a95ac8db06f28488
titrom <71824652+titrom@users.noreply.github.com>
2021-12-14 14:22:18 +0300

    Initial commit

---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMove : MonoBehaviour
+{
+    public float Spead = 1.0f;
+    public InputAction Controle;
+
+
+    private Vector2 move = Vector2.zero;
+    private Rigidbody2D rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void OnEnable()
+    {
+        Controle.Enable();
+    }
+
+
+    private void OnDisable()
+    {
+        Controle.Disable();
+    }
+
+
+    private void Update()
+    {
+        move = Controle.ReadValue<Vector2>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(move.x * Spead, move.y * Spead);
+    }
+
+}
